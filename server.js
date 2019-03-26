51b7bf1d9a0c9d7d5fdb63676b50f0595a7b7fcd
Jason Slavin <jslavin@marketo.com>
2019-03-26 10:30:18 -0700

    Preparing build for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const path = require('path');
 const port = process.env.PORT || 5000;
 const dotenv = require('dotenv');
 
@@ -12,8 +13,26 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 app.use(cors());
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
+//start server
+app.listen(port, (req, res) => {
+  console.log( `server listening on port: ${port}`);
+})
 
 // create a GET route
 app.use('/api', apiRouter);
